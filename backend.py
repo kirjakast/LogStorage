9e937d103c4dc1bf5a91ea6a056cc1a9cded2434
kirjakast <janek@kirjakast.eu>
2018-02-04 22:05:16 +0200

    Lisandusid read Tüüp ja Märkused.

--- backend.py
+++ backend.py
@@ -3,14 +3,14 @@ import sqlite3
 def connect():
     conn=sqlite3.connect("baas.db")
     cur=conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS logpath (id INTEGER PRIMARY KEY, application text, environment text, log_path text)")
+    cur.execute("CREATE TABLE IF NOT EXISTS logpath (id INTEGER PRIMARY KEY, application text, environment text, log_path text, log_type text, log_note text)")
     conn.commit()
     conn.close()
 
-def insert(application,environment,log_path):
+def insert(application,environment,log_path,log_type,log_note):
     conn=sqlite3.connect("baas.db")
     cur=conn.cursor()
-    cur.execute("INSERT INTO logpath VALUES (NULL,lower(?),lower(?),lower(?))",(application,environment,log_path))
+    cur.execute("INSERT INTO logpath VALUES (NULL,lower(?),lower(?),lower(?),lower(?),lower(?))",(application,environment,log_path,log_type,log_note))
     conn.commit()
     conn.close()
 
@@ -22,7 +22,7 @@ def view():
     conn.close()
     return rows
 
-def search(application="",environment="",log_path=""):
+def search(application="",environment="",log_path="",log_type="",log_note=""):
     conn=sqlite3.connect("baas.db")
     cur=conn.cursor()
 
@@ -33,6 +33,10 @@ def search(application="",environment="",log_path=""):
         dict['environment'] = environment
     if log_path != "":
         dict['log_path'] = log_path
+    if log_type != "":
+        dict['log_type'] = log_type
+    if log_note != "":
+        dict['log_note'] = log_note
 
     t = []
     l = []
@@ -40,7 +44,11 @@ def search(application="",environment="",log_path=""):
             t.append(k)
             l.append(v)
 
-    if len(l) == 3:
+    if len(l) == 5:
+        cur.execute("SELECT * FROM logpath WHERE " + t[0]+"=? AND " + t[1]+"=? AND "+ t[2]+"=? AND "+ t[3]+"=? AND "+ t[4]+"=? ORDER BY environment DESC", (l[0],l[1],l[2],l[3],l[4],))
+    elif len(l) == 4:
+        cur.execute("SELECT * FROM logpath WHERE " + t[0]+"=? AND " + t[1]+"=? AND "+ t[2]+"=? AND "+ t[3]+"=? ORDER BY environment DESC", (l[0],l[1],l[2],l[3],))
+    elif len(l) == 3:
         cur.execute("SELECT * FROM logpath WHERE " + t[0]+"=? AND " + t[1]+"=? AND "+ t[2]+"=? ORDER BY environment DESC", (l[0],l[1],l[2],))
     elif len(l) == 2:
         cur.execute("SELECT * FROM logpath WHERE " + t[0]+"=? AND " + t[1]+"=? ORDER BY environment DESC" , (l[0],l[1],))
@@ -57,10 +65,10 @@ def delete(id):
     conn.commit()
     conn.close()
 
-def update(id,application,environment,log_path):
+def update(id,application,environment,log_path,log_type,log_note):
     conn=sqlite3.connect("baas.db")
     cur=conn.cursor()
-    cur.execute("UPDATE logpath SET application=lower(?), environment=lower(?), log_path=lower(?) WHERE id=?",(application,environment,log_path,id))
+    cur.execute("UPDATE logpath SET application=lower(?), environment=lower(?), log_path=lower(?), log_type=lower(?), log_note=lower(?) WHERE id=?",(application,environment,log_path,log_type,log_note,id))
     conn.commit()
     conn.close()
 connect()
--- frontend.py
+++ frontend.py
@@ -25,6 +25,10 @@ def get_selected_row(event):
         e2.insert(END,selected_tuple[2])
         e3.delete(0,END)
         e3.insert(END,selected_tuple[3])
+        e4.delete(0,END)
+        e4.insert(END,selected_tuple[4])
+        e5.delete(0,END)
+        e5.insert(END,selected_tuple[5])
     except IndexError:
         pass
 
@@ -35,19 +39,26 @@ def view_command():
 
 def search_command():
     list1.delete(0,END)
-    for row in backend.search(application_text.get(),environment_text.get(),path_text.get()):
+    for row in backend.search(application_text.get(),environment_text.get(),path_text.get(),type_text.get(),note_text.get()):
         list1.insert(END,row)
 
 def add_command():
-    backend.insert(application_text.get(),environment_text.get(),path_text.get())
+    backend.insert(application_text.get(),environment_text.get(),path_text.get(),type_text.get(),note_text.get())
     list1.delete(0,END)
-    list1.insert(END,(application_text.get(),environment_text.get(),path_text.get()))
+    list1.insert(END,(application_text.get(),environment_text.get(),path_text.get(),type_text.get(),note_text.get()))
 
 def delete_command():
     backend.delete(selected_tuple[0])
 
 def update_command():
-    backend.update(selected_tuple[0],application_text.get(),environment_text.get(),path_text.get())
+    backend.update(selected_tuple[0],application_text.get(),environment_text.get(),path_text.get(),type_text.get(),note_text.get())
+
+def zero_command():
+    e1.delete(0,END)
+    e2.delete(0,END)
+    e3.delete(0,END)
+    e4.delete(0,END)
+    e5.delete(0,END)
 
 window=Tk()
 window.wm_title("LogStorage")
@@ -61,6 +72,12 @@ l2.grid(row=1,column=0,sticky=W)
 
 l3=Label(window,text="Asukoht")
 l3.grid(row=2,column=0,sticky=W)
+
+l4=Label(window,text="Tüüp")
+l4.grid(row=3,column=0,sticky=W)
+
+l5=Label(window,text="Märkmed")
+l5.grid(row=4,column=0,sticky=W)
 
 #Entries
 #Rakendus
@@ -78,13 +95,23 @@ path_text=StringVar()
 e3=Entry(window,textvariable=path_text,width=100)
 e3.grid(row=2,column=1,sticky=W)
 
+#Tüüp
+type_text=StringVar()
+e4=Entry(window,textvariable=type_text)
+e4.grid(row=3,column=1,sticky=W)
+
+#Märkused
+note_text=StringVar()
+e5=Entry(window,textvariable=note_text,width=100)
+e5.grid(row=4,column=1,sticky=W)
+
 #Loome lihtsalt Listboxi
-list1=Listbox(window,height=15,width=110)
-list1.grid(row=3,column=0,rowspan=6,columnspan=2)
+list1=Listbox(window,height=12,width=110)
+list1.grid(row=5,column=0,rowspan=8,columnspan=2)
 
 #1. Loome lihtsalt skrollbari
 sb1=Scrollbar(window)
-sb1.grid(row=3,column=2,rowspan=6)
+sb1.grid(row=5,column=2,rowspan=8)
 
 #2. Seadistame äsja loodud listboxi ja scrollbari, et nad mõistakisd üksteist
 list1.configure(yscrollcommand=sb1.set) #Listi y telje küljes on eelnevalt loodud scrollbar
@@ -95,23 +122,26 @@ list1.bind('<<ListboxSelect>>',get_selected_row)
 
 
 #------------------------------NUPUD----------------------------------------------
-b1=Button(window,text="Vaata",width=12,command=view_command)
-b1.grid(row=3,column=3)
+b1=Button(window,text="Vaata kõiki",width=12,command=view_command)
+b1.grid(row=5,column=3,sticky=N)
 
-b2=Button(window,text="Otsi",width=12,command=search_command)
-b2.grid(row=4,column=3)
+b2=Button(window,text="Nulli",width=12,command=zero_command)
+b2.grid(row=6,column=3,sticky=N)
 
-b3=Button(window,text="Lisa",width=12,command=add_command)
-b3.grid(row=5,column=3)
+b3=Button(window,text="Otsi",width=12,command=search_command)
+b3.grid(row=7,column=3,sticky=N)
 
-b4=Button(window,text="Uuenda",width=12, command=update_command)
-b4.grid(row=6,column=3)
+b4=Button(window,text="Lisa",width=12,command=add_command)
+b4.grid(row=8,column=3,sticky=N)
 
-b5=Button(window,text="Kustuta",width=12,command=delete_command)
-b5.grid(row=7,column=3)
+b5=Button(window,text="Muuda",width=12, command=update_command)
+b5.grid(row=9,column=3,sticky=N)
 
-b6=Button(window,text="Sulge",width=12,command=window.destroy)
-b6.grid(row=8,column=3)
+b6=Button(window,text="Kustuta",width=12,command=delete_command)
+b6.grid(row=10,column=3,sticky=N)
+
+b7=Button(window,text="Sulge",width=12,command=window.destroy)
+b7.grid(row=11,column=3,sticky=N)
 
 
 
